4d60dab3b89df8608f75711c7c38e09427673b78
lixiaisaaa <1067907540@qq.com>
2022-11-22 22:14:28 -0700

    Final

--- CourseScraper/Form1.Designer.cs
+++ CourseScraper/Form1.Designer.cs
@@ -31,45 +31,161 @@
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.courseText = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(116, 59);
+            this.button1.Location = new System.Drawing.Point(44, 33);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(131, 58);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Scrape Courses";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(149, 128);
+            this.richTextBox1.Location = new System.Drawing.Point(44, 123);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(464, 266);
+            this.richTextBox1.Size = new System.Drawing.Size(573, 315);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(374, 51);
+            this.button2.Location = new System.Drawing.Point(181, 33);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(131, 58);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "Find Erollments";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(374, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Year";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(374, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Semester";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(437, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(238, 23);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(437, 73);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(238, 23);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+           
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(374, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Course To Scrape";
+            // 
+            // courseText
+            // 
+            this.courseText.Location = new System.Drawing.Point(477, 6);
+            this.courseText.Name = "courseText";
+            this.courseText.Size = new System.Drawing.Size(198, 23);
+            this.courseText.TabIndex = 8;
+            this.courseText.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(657, 140);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 48);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(658, 230);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 48);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "HTML";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Course number been scraped";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(212, 97);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(28, 23);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.courseText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -78,5 +194,15 @@
         private Button button1;
         private RichTextBox richTextBox1;
         private Button button2;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private ComboBox comboBox1;
+        private Label label3;
+        private TextBox courseText;
+        private Button button3;
+        private Button button4;
+        private Label label4;
+        private TextBox textBox3;
     }
 }
--- CourseScraper/Form1.cs
+++ CourseScraper/Form1.cs
@@ -1,10 +1,357 @@
+﻿/**
+* Author:    Xia Li
+* Date:      11/22/2022
+* Course:    CS 4540, University of Utah, School of Computing
+* Copyright: CS 4540 and Xia Li and Wenlin Li - This work may not be copied for use in Academic Coursework.
+*
+* I, Xia Li, certify that I wrote this code from scratch and did 
+* not copy it in part or whole from another source.  Any references used 
+* in the completion of the assignment are cited in my README file and in
+* the appropriate method header.
+*
+* Web Scraper for University of Utah course page
+*
+*/
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Diagnostics.Metrics;
+using System.Security.Policy;
+using System.Text;
+using Application = System.Windows.Forms.Application;
+using Keys = OpenQA.Selenium.Keys;
+
+
 namespace CourseScraper
 {
+
+    
     public partial class Form1 : Form
     {
+        ChromeDriver _driver;
+        string year;
+        string semester;
+        int limit;
+        List<string> classNumber;
+        List<string> subject;
+        List<string> catagorieNumber;
+        List<string> section;
+        List<string> title;
+        List<string> enrollment;
+        List<string> waitList;
+        List<string> currentEnrolled;
+        List<string> seatingAvailiable;
+        List<string> pID;
+        List<string> credits;
+        List<string> courseDescription;
+        string description;
+        string credit;
+        
+
         public Form1()
         {
             InitializeComponent();
+            if (_driver == null)
+            {
+                _driver = new ChromeDriver();
+                limit = 0;
+            }
+            
+            comboBox1.Items.Add("Spring");
+            comboBox1.Items.Add("Fall");
+            comboBox1.Items.Add("Summer");
+ 
+            limit = 0;
+            button3.Enabled = false;
+            credit = "";
+            description = "";
+            classNumber = new List<string>();
+            subject = new List<string>();
+            catagorieNumber = new List<string>();
+            section = new List<string>();
+            title = new List<string>();
+            enrollment = new List<string>();
+            waitList = new List<string>();
+            currentEnrolled = new List<string>();
+            seatingAvailiable = new List<string>();
+            pID = new List<string>();
+            credits = new List<string>();
+            courseDescription = new List<string>();
+            Application.ApplicationExit += new EventHandler(this.OnApplicationExit);
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        //Scrape
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "" || courseText.Text == "")
+            {
+                MessageBox.Show("The course or semester is missing, please make sure you input them");
+                return;
+            }
+
+            if (limit == 100)
+            {
+                richTextBox1.Text += "The limit of scraping has been exceeded" + "\r\n";
+                return;
+            }
+            string[] courseName = courseText.Text.ToUpper().Split("CS");
+            if (Int32.Parse(courseName[1]) >= 6999 || Int32.Parse(courseName[1]) <= 1000)
+            {
+                richTextBox1.Text += "Please search the course between 1000 to 6999" + "\r\n";
+                return;
+            }
+            string url = "https://catalog.utah.edu/#/home";
+
+            scrape(url);
+        }
+
+        public void scrape(String url)
+        {
+            _driver.Navigate().Refresh();
+            _driver.Navigate().GoToUrl(url);
+            
+                try
+                {
+                    _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                    var input_the_search = _driver.FindElement(By.XPath("//*[@id=\"top\"]/div/div[2]/span/div/div/div/input"));
+                    _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                    input_the_search.SendKeys(courseText.Text.ToString());
+                    _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                    input_the_search.SendKeys(Keys.Return);
+                    var click_on_course = _driver.FindElement(By.PartialLinkText(courseText.Text.ToUpper()));
+                    _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                    click_on_course.Click();
+                    var course_Division = _driver.FindElements(By.ClassName("course-view__pre___2VF54"));
+                    int index = 0;
+                    var course_Description_Text = "";
+                    credit = _driver.FindElement(By.XPath("//*[@id=\"__KUALI_TLP\"]/div/div[2]/div[2]/span/div/div/div/div/div/div")).Text;
+                    int preCheck = 0;
+                    foreach (var c in course_Division)
+                    {
+                    
+                    
+                    if (index == 2)
+                    {
+                        var course_Description = c.FindElement(By.TagName("div")).Text;
+                        if (course_Description.Contains("Prerequisites"))
+                        {
+                            preCheck = 1;
+                        }
+                        else
+                        {
+                            description = course_Description;
+                            course_Description_Text = courseText.Text + " Course Description: " + "\r\n" + course_Description + "\r\n";
+                        }
+                    }
+
+                    if(preCheck == 1)
+                    {
+                        if (index == 3)
+                        {
+                            var course_Description = c.FindElement(By.TagName("div")).Text;
+                            description = course_Description;
+                            course_Description_Text = courseText.Text + " Course Description: " + "\r\n" + course_Description + "\r\n";
+                            preCheck = 0;
+                        }
+                    }
+                       
+                        
+                        if (index == 4)
+                        {
+                            var components = c.FindElements(By.TagName("div"));
+                            foreach (var component in components)
+                            {
+                                if (component.Text.ToUpper().Contains("LECTURE") || component.Text.ToUpper().Contains("SEMINAR"))
+                                {
+                                    course_Description_Text += "Course Component: " + "\r\n" + component.Text + "\r\n";
+                                    break;
+                                }
+                                else
+                                {
+                                richTextBox1.Text += "This course is not Lecture or Seminar";
+                                return;
+                                }
+                            }      
+                        }
+                        index++;
+                    }
+                    richTextBox1.Text += course_Description_Text;
+                    limit++;
+                    textBox3.Text = limit.ToString();
+                }
+                catch
+                {
+                    richTextBox1.Text += "The course you are looking for is not found";
+                }
+            }
+            
+        private void getFullEnroll(String url)
+        {
+            _driver.Navigate().GoToUrl(url);
+            _driver.Manage().Window.Size = new Size(1920, 1080);//get full size window so we can scrape
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            var seating_link_click = _driver.FindElement(By.LinkText("Seating availability for all CS classes"));
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            seating_link_click.Click();
+            var table = _driver.FindElement(By.TagName("table"));
+            var body = table.FindElements(By.XPath("//tbody//tr"));
+            int i = 1; 
+            foreach (var r in body)
+            {
+                string section = _driver.FindElement(By.XPath("//*[@id=\"seatingAvailabilityTable\"]/tbody/tr[" + i.ToString() + "]" + "/td[4]")).Text;
+                string cl = _driver.FindElement(By.XPath("//*[@id=\"seatingAvailabilityTable\"]/tbody/tr[" + i.ToString() + "]" + "/td[3]")).Text;
+
+                var classNum = _driver.FindElement(By.XPath("//*[@id=\"seatingAvailabilityTable\"]/tbody/tr[" + i.ToString() + "]" + "/td[1]")).Text;
+                _driver.Navigate().GoToUrl(url);
+                var component = _driver.FindElement(By.XPath("//*[@id=\"" + classNum + "\"]/div[1]/div[2]/ul/li[3]/span")).Text;
+
+                if (section == "001" && (component == "Lecture" || component == "Seminar") && (Int32.Parse(cl) > 1000 || Int32.Parse(cl) < 6999))
+                {
+                   
+                    //Course c = new Course();
+                    seating_link_click = _driver.FindElement(By.LinkText("Seating availability for all CS classes"));
+                    seating_link_click.Click();
+                    string cNum = _driver.FindElement(By.XPath("//*[@id=\"seatingAvailabilityTable\"]/tbody/tr[" + i.ToString() + "]" + "/td[1]")).Text;
+                    string sbj = _driver.FindElement(By.XPath("//*[@id=\"seatingAvailabilityTable\"]/tbody/tr[" + i.ToString() + "]" + "/td[2]")).Text;
+                    string t = _driver.FindElement(By.XPath("//*[@id=\"seatingAvailabilityTable\"]/tbody/tr[" + i.ToString() + "]" + "/td[5]")).Text;
+                    string e = _driver.FindElement(By.XPath("//*[@id=\"seatingAvailabilityTable\"]/tbody/tr[" + i.ToString() + "]" + "/td[6]")).Text;
+                    string wList = _driver.FindElement(By.XPath("//*[@id=\"seatingAvailabilityTable\"]/tbody/tr[" + i.ToString() + "]" + "/td[7]")).Text;
+                    string curr = _driver.FindElement(By.XPath("//*[@id=\"seatingAvailabilityTable\"]/tbody/tr[" + i.ToString() + "]" + "/td[8]")).Text;
+                    string seats = _driver.FindElement(By.XPath("//*[@id=\"seatingAvailabilityTable\"]/tbody/tr[" + i.ToString() + "]" + "/td[9]")).Text;
+                    classNumber.Add(cNum);
+                    subject.Add(sbj);
+                    catagorieNumber.Add(cl);
+                    this.section.Add(section);
+                    this.title.Add(t);
+                    enrollment.Add(e);
+                    waitList.Add(wList);
+                    currentEnrolled.Add(curr);
+                    seatingAvailiable.Add(seats);
+
+                    courseText.Text = sbj + cl;
+
+                    button1.PerformClick();
+
+                    credits.Add(credit);
+                    courseDescription.Add(description);
+
+                    _driver.Navigate().GoToUrl(url);
+
+                    var courseNum = _driver.FindElement(By.XPath("//*[@id=\"" + cNum + "\"]/div[1]/div[2]/ul/li[2]/span/a"));
+                    string pid = courseNum.GetAttribute("href").Split("/")[3];
+                    pID.Add(pid);
+                    richTextBox1.Text += comboBox1.SelectedItem.ToString() + "," + textBox1.Text + "," + sbj + "," + cl + "," + t + "," + pid + "," + credit + "," + e + "," + description + "\r\n";
+                    seating_link_click = _driver.FindElement(By.LinkText("Seating availability for all CS classes"));
+                    seating_link_click.Click();
+                    credit = "";
+                    description = "";
+                }
+                else
+                {
+                    seating_link_click = _driver.FindElement(By.LinkText("Seating availability for all CS classes"));
+                    seating_link_click.Click();
+                    credit = "";
+                    description = "";
+                }
+                i++;
+            }
+            button3.Enabled = true;
+        }
+
+        //Find Enrollment
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == ""  || comboBox1.Text == "")
+            {
+                MessageBox.Show("The semester or year is missing, please make sure you input them");
+                return;
+            }
+            string semesters = comboBox1.SelectedItem.ToString();
+            switch (semesters)
+            {
+                case "Spring":
+                    semester = "4";
+                    break;
+                case "Summer":
+                    semester = "6";
+                    break;
+                case "Fall":
+                    semester = "8";
+                    break;
+            }
+            year = textBox1.Text.Trim().Substring(textBox1.Text.Length - 3, textBox1.Text.Length - 1).ToString();
+            year = year.Substring(year.Length - 2, year.Length - 1);
+            string url = "https://student.apps.utah.edu/uofu/stu/ClassSchedules/main/" + "1" + year + semester + "/class_list.html?subject=CS";
+            getFullEnroll(url);
+             
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+        }
+
+        
+
+        //Save 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savefile = new SaveFileDialog();
+            var sb = new StringBuilder();
+            savefile.Filter = "CSV file (*.csv)|*.csv| All Files (*.*)|*.*";
+            savefile.FilterIndex = 2;
+            savefile.RestoreDirectory = true;
+            sb.AppendLine("Course Semester,Course Year,Course Dept,Course Number,Course Title,Course Instructors UNID,Course Credits,Course Enrollment,Course Description");
+            for (int i = 0; i < classNumber.Count; i++)
+            {
+                sb.AppendLine(comboBox1.SelectedItem.ToString() + "," + textBox1.Text + "," + subject[i] + "," + catagorieNumber[i] + "," + title[i] + "," + pID[i] + "," + credit[i] + "," + enrollment[i] + "," + courseDescription[i]);
+            }
+
+            if (savefile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    File.WriteAllText(savefile.FileName, sb.ToString());
+                }catch(Exception error)
+                {
+                    MessageBox.Show(error.ToString());
+                }
+                
+            }
+        }
+        private void OnApplicationExit(object sender, EventArgs e)
+        {
+            _driver.Quit();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
     }
 }
